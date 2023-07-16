bc885cf5cf812af294fa94eb279cee98e6989971
VlaLyas <vova.lyashik@gmail.com>
2023-07-16 12:13:10 +0300

    2

---
+++ hypr/gkt-themes.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+THEME="Arc-Dark"
+ICONS="Tela-dark"
+FONT="Source Code Pro Nerd 12"
+CURSOR="Breeze"
+
+SCHEMA="gsettings set org.gnome.desktop.interface"
+
+apply_themes(){
+    ${SCHEMA} gtk-theme "$THEME"
+    ${SCHEMA} icon-theme "$ICONS"
+    ${SCHEMA} cursor-theme "$CURSOR"
+    ${SCHEMA} font-name "$FONT"
+}
+
+apply_themes
