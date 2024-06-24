c556b6b3e3d8927ca4a220d9b76eeeb40baa113e
VolLyas <132204343+VolLyas@users.noreply.github.com>
2024-06-24 12:25:01 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cp -a * ~/.config/
 cd ~/.config/slstatus-1.0 && make clean install
-cd ~/.config/dwm-6.5 && make celan install
+cd ~/.config/dwm-6.5 && make clean install
 echo export QT_QPA_PLATFORMTHEME=qt5ct >> ~/.profile
 echo export XDG_CONFIG_HOME=\"\$HOME/.config\" >> ~/.profile
