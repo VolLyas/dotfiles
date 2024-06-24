7bc25043952760c1fb7885ff6b36d12f96868008
VolLyas <132204343+VolLyas@users.noreply.github.com>
2024-06-24 12:26:06 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,3 +4,4 @@ cd ~/.config/slstatus-1.0 && make clean install
 cd ~/.config/dwm-6.5 && make clean install
 echo export QT_QPA_PLATFORMTHEME=qt5ct >> ~/.profile
 echo export XDG_CONFIG_HOME=\"\$HOME/.config\" >> ~/.profile
+echo exec dbus-run-session dwm >> ~/.xinitrc
