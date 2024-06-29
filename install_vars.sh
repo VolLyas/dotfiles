1d65e0f9c15c2762c348a3185a6a071b4814ad55
VolLyas <vova.lyashik@gmail.com>
2024-06-29 17:00:56 +0300

    upd 29.06

--- install_vars.sh
+++ install_vars.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 echo export QT_QPA_PLATFORMTHEME=qt5ct >> ~/.profile
 echo export XDG_CONFIG_HOME=\"\$HOME/.config\" >> ~/.profile
+
+echo pactl set-sink-mute @DEFAULT_SINK@ off >> ~/.xinitrc
+echo pactl set-sink-volume @DEFAULT_SINK@ 60% >> ~/.xinitrc
+echo volumeicon & >> ~/.xinitrc
+echo xsetroot -cursor_name left_ptr & >> ~/.xinitrc
+echo dunst & >> ~/.xinitrc
 echo exec dbus-run-session dwm >> ~/.xinitrc
