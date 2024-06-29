71da99c17c5c91b378f32ea70ca405462103c247
VolLyas <vova.lyashik@gmail.com>
2024-06-29 16:12:13 +0300

    upd 29.06

--- dwm-6.5/autostart.sh
+++ dwm-6.5/autostart.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 blueman-applet &
+~/.config/dwm-6.5/status.sh &
+picom -b &
+nitrogen --restore &
+/usr/lib64/polkit-gnome/polkit-gnome-authentication-agent-1 &
+setxkbmap us,ua -option grp:alt_shift_toggle &
